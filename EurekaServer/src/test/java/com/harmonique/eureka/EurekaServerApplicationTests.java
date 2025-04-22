f2c76d730f17d91a019bb8053831ceabecaeabd5
Vedant <ved.shinde77@gmail.com>
2025-04-22 18:44:17 +0530

    commit try

---
+++ EurekaServer/src/test/java/com/harmonique/eureka/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harmonique.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
